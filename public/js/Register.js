5d0426ed3c6ca1108cf1fe77b60408b0dac76e44
Brandon2707 <brandontorresvargas07@gmail.com>
2025-08-25 08:17:05 -0600

    Proyecto completado

--- public/js/Register.js
+++ public/js/Register.js
@@ -1,22 +1,22 @@
-document.getElementById("registerForm").addEventListener("submit", function(e) {
-  e.preventDefault(); 
-  const username = document.getElementById("NombreRe").value.trim();
-  const password = document.getElementById("ContraseñaRe").value.trim();
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("NombreRe").value.trim();
+    const password = document.getElementById("ContraseñaRe").value.trim();
 
-  // Validación: campos obligatorios
-  if (!username || !password) {
-    alert("Por favor llena todos los campos");
-    return;
-  }
+    // Validación: campos obligatorios
+    if (!username || !password) {
+        alert("Por favor llena todos los campos");
+        return;
+    }
 
-  // Creamos el objeto usuario
-  const user = { username, password };
+    // Creamos el objeto usuario
+    const user = { username, password };
 
-  // Guardamos en localStorage (como JSON)
-  localStorage.setItem("user", JSON.stringify(user));
+    // Guardamos en localStorage (como JSON)
+    localStorage.setItem("user", JSON.stringify(user));
 
-  alert("Usuario registrado con éxito ✅");
+    alert("Usuario registrado con éxito ✅");
 
-  // Redirige al login
-  window.location.href= "../pages/LoginRevi.html"
+    // Redirige al login
+    window.location.href = "../pages/LoginRevi.html"
 });
